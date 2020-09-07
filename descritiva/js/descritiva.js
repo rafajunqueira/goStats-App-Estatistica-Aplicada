bad6c480f160601307339cd5fb5602d0928fc719
rafajunqueira <rafa.rjunqueira@gmail.com>
2020-09-07 18:43:12 -0300

    qualitativa-ok

--- descritiva/js/descritiva.js
+++ descritiva/js/descritiva.js
@@ -6,14 +6,13 @@ btnCalc.onclick = () => {
   //Rastreando valores e nomeVariavel(coluna 1) inserida
   const inputDados = document.querySelector('#coleta_de_dados').value
   const nomeVariavel = document.querySelector('.nomeVariavel').value
-
-
+  const optSelected = document.querySelector('input[name="nome_da_variavel"]:checked').value;
   // criando uma nova variável com o que foi analisado de freq. na função
-  let objFrequencia = geraFrequencia(inputDados)
+  let objFrequencia = geraFrequencia(inputDados, optSelected)
   let coluna1 = Object.keys(objFrequencia) // Object.keys pega o nome do atributo
   let coluna2 = Object.values(objFrequencia) // Object.values pega o VALOR do atributo
 
-  
+
   // processo de geração da tabela : (achei melhor não usar a template string como havia falado)
   var html = `<table border='1'>
               <tr>
@@ -37,10 +36,13 @@ btnCalc.onclick = () => {
 }
 
 // Função que separa as vírgulas e gera freq.; ela recebe como parametro os dados inseridos.
-function geraFrequencia(dados_inseridos) {
+function geraFrequencia(dados_inseridos, tipoVariavel) {
   var pattern = /\w+/g,
     string = dados_inseridos,
     matchedWords = string.match(pattern);
+  if (tipoVariavel == 1) {
+    matchedWords.sort()
+  }
   var counts = matchedWords.reduce(function (stats, word) {
     if (stats.hasOwnProperty(word)) {
       stats[word] = stats[word] + 1;
@@ -49,6 +51,7 @@ function geraFrequencia(dados_inseridos) {
     }
     return stats;
   }, {});
+
   return counts
 }
 
