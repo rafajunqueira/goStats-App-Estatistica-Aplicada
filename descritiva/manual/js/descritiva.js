0e9cb192bedcde983d0e80f43b6561065aebba9c
testes <thiagofcjr@gmail.com>
2020-09-11 20:34:35 -0300

    commit com tabela Simples + Contínua sendo geradas

--- descritiva/manual/js/descritiva.js
+++ descritiva/manual/js/descritiva.js
@@ -8,11 +8,11 @@ btnCalc.onclick = () => {
   const nomeVariavel = document.querySelector('.nomeVariavel').value
   // criando uma nova variável com o que foi analisado de freq. na função
 
-  let Array = inputDados.split(';'); // Separando ';' dos dados
+  let arrayOriginal = inputDados.split(';'); // Separando ';' dos dados
 
-  let ArrayOrdenado = Array.sort(ordenaArray) // Ordenando array
+  let arrayOrdenado = arrayOriginal.sort(ordenaArray) // Ordenando array
 
-  let objFrequencia = geraFrequencia(ArrayOrdenado) //Gerando objeto com array usado
+  let objFrequencia = geraFrequencia(arrayOrdenado) //Gerando objeto com array usado
 
   // valorVariavel é coluna1
   let valorVariavel = Object.keys(objFrequencia) // Object.keys pega o nome do atributo
@@ -28,8 +28,18 @@ btnCalc.onclick = () => {
                 <th>Frequência</th>
               </tr>`
 
-  //abaixo geramos a tabelaSimples (quali. nominal, ordinal, quanti. discreta)
-  tabela = cabecalho + tabelaSimples(valorVariavel, freqSimples)
+
+  /*se +10 ELEMENTOS NUMERAIS forem inseridos geramos a tabelaContinua
+  SENÃO
+  somente geramos a tabelaSimples
+  */
+  let tabelaEscolhida = qualtabela(inputDados)
+  if ((valorVariavel.length >= 10) && (tabelaEscolhida == 'tabelaContinua')) {
+    tabela = cabecalho + tabelaContinua(arrayOrdenado)
+  } else {
+    tabela = cabecalho + tabelaSimples(valorVariavel, freqSimples)
+  }
+
 
   // aqui adicionamos o que rolou no for dentro de uma div de resultado
   const domTabela = document.querySelector('.tabelas')
@@ -40,6 +50,21 @@ btnCalc.onclick = () => {
 
 
 /******************** ÁREA DE FUNÇÕES: ********************/
+
+let qualtabela = (inputDados) => {
+
+  // tirando caracteres especiais e depois espaço:
+  inputDados.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '')
+  inputDados.replace(/\s/g, '')
+
+  if (isNaN(parseFloat(inputDados))) { // se for string tabelaSimples em uso
+    return 'tabelaSimples'
+  } else {
+    return 'tabelaContinua' // se for number tabelaContinua em uso
+  }
+}
+
+
 let wordCounter = {}; // contador de elementos (int ou string)
 
 
@@ -69,8 +94,85 @@ let geraFrequencia = (wordArray) => {
 }
 
 
+let tabelaContinua = (arrayOrdenado) => {
+
+  //achando o priElemento do arrayOrdenado
+  let priElemento = parseInt(arrayOrdenado.slice(0, 1))
+
+  //achando o ultElemento do arrayOrdenado
+  let ultElemento = parseInt(arrayOrdenado.slice(-1))
+
+  //calc de Amplitude Total
+  let AmpliTotal = ultElemento - priElemento
+
+  //achando o index do 1º numero a ser pesquisado (1º maior que AmpliTotal)
+  let indexInicial = arrayOrdenado.findIndex(elemento => elemento > AmpliTotal)
+
+  //calc de qtd de Classes (qtd de lihas da tabela)
+  let qtdClasses = Math.sqrt(arrayOrdenado.length)
+
+  //arredondando qtd de Classes e voltando para o numero anterior
+  qtdClasses = Math.round(qtdClasses) - 1
+
+  //calc de intervalo de Classe
+  let intClasse = ''
+
+  for (let i = indexInicial; i <= arrayOrdenado.length; i++) {
+    //Encontrando o intervalo de Classe: 
+    intClasse = arrayOrdenado[i] / qtdClasses
+
+    //Se o intervalo de Classe for redondo, achamos o ideal
+    if (intClasse === Math.round(intClasse)) {
+      break
+    }
+
+    /*Se chegar ao final do array e não encontrar intervalo de Classe redondo,
+    então acrescentamos +1 no qtdClasses e reiniciamos a busca*/
+    if (i === arrayOrdenado.length) {
+      qtdClasses += 1
+      i = indexInicial
+    }
+  }
+
+  let corpo = ''
+
+  // lembre-se priElemento e ultElemento vem do arrayOrdenado
+  for (let i = priElemento; i <= ultElemento; i += intClasse) {
+
+    if (i + intClasse >= ultElemento) {
+
+      corpo += `<tr>
+                <td>${i} |--- ${i + intClasse}</td>
+                <td>${qtdIndexEntre(arrayOrdenado, i, ultElemento) + 1}</td>
+                </tr>` // esse +1 serve para corrigir a falta
+
+    } else {
+
+      corpo += `<tr>
+                <td>${i} |--- ${i + intClasse}</td>
+                <td>${qtdIndexEntre(arrayOrdenado, i, i + intClasse)}</td>
+                </tr>`
+    }
+  }
+
+
+  return corpo + '</table>'
+}
+
+
+let qtdIndexEntre = (array, inicial, final) => {
+
+  let indexNumInicial = array.findIndex(element => element >= inicial)
+  let indexNumFinal = array.findIndex(element => element >= final)
+
+  let qtdEntre = indexNumFinal - indexNumInicial
+
+  return qtdEntre
+}
+
+
 //gerador de tabelaSimples (quali. nominal, ordinal, quanti. discreta)
-function tabelaSimples(coluna1, coluna2) {
+let tabelaSimples = (coluna1, coluna2) => {
   let corpo = ''
 
   for (let i = 0; i < coluna1.length; i++) {
