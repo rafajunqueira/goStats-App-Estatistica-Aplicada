1757af92f04609ffd8d61bcc3fdb9ca68a4f98a9
testes <thiagofcjr@gmail.com>
2020-09-16 22:44:32 -0300

    elaboração: modal escolha de planilha

--- descritiva/importar/js/importa.js
+++ descritiva/importar/js/importa.js
@@ -11,14 +11,49 @@ let data = [{
     "abc": "sdef"
 }]
 
+
+function userEscPlanilha([...nomesPlanilhas]) {
+
+    let verificaPlanilha = document.querySelector('.verificaPlanilha')
+    let div = ''
+    for (let i = 0; i < nomesPlanilhas.length; i++) {
+        div += `<input type="radio" name="gender" value="${nomesPlanilhas[i]}" checked><label>${nomesPlanilhas[i]}</label><br>`
+    }
+    console.log('div :>> ', div);
+
+    verificaPlanilha.innerHTML = `<div class="form-dados" style="position: fixed">
+    <div class="container descritiva-main form-custom formulario">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 text-center">
+                <blockquote class="blockquote">
+                    <p class="mb-0">Certo, detectamos que o inserido possui ${nomesPlanilhas.length} planilhas, marque abaixo qual delas vamos fazer análise:</p>
+                    <footer class="blockquote-footer">A análise será feita automaticamente</footer></blockquote>${div}
+                    <a class="btn btn-outline-secondary btn-custom"
+                    href="./manual/manual.html" style="width: 77%;">Já escolhi a Planilha!</a>
+            </div>
+        </div>
+    </div>
+</div>`
+}
+
 document.querySelector('#button').addEventListener("click", () => {
     XLSX.utils.json_to_sheet(data, 'out.xlsx')
     if (selectedFile) {
         let fileReader = new FileReader()
         fileReader.readAsBinaryString(selectedFile)
-        fileReader.onload = (event) => {
+        fileReader.onload = async (event) => {
             let data = event.target.result
             let workbook = XLSX.read(data, { type: "binary" })
+
+            let qtdPlanilhas = workbook.SheetNames.length
+            let nomesPLanilhas = workbook.SheetNames
+
+            if (qtdPlanilhas > 1) {
+                await userEscPlanilha(nomesPLanilhas)
+            }
+
+            console.log('qtdPlanilhas :>> ', qtdPlanilhas);
+
             let planilhaEscolhida = workbook.Sheets["Modelo Discreto"]
             let tamanho = Object.values(planilhaEscolhida).length
             tamanho -= 2 // nesse 2 aqui descontamos tab de planilha (formatação excel)
